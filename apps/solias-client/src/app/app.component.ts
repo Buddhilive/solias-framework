b30c35ec65d4903aaa5c3cb286f9a3f3e5bad726
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-30 07:12:48 +0530

    Setup layout component and routing

--- apps/solias-client/src/app/app.component.ts
+++ apps/solias-client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [LayoutComponent],
   selector: 'solias-root',
-  template: `<app-nx-welcome></app-nx-welcome> <router-outlet></router-outlet>`,
+  template: `<solias-layout />`,
   styles: [],
 })
 export class AppComponent {}
