6dff81fc8152056359fb8241f02e402860759513
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-31 07:17:32 +0530

    Add footer component

--- apps/solias-client/src/app/layout/layout.component.ts
+++ apps/solias-client/src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'solias-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
