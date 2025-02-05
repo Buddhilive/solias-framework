6a677dc00012765abca88d4f65aa77d463b3b205
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-05 22:22:00 +0530

    Restructure Navigation menu

--- apps/solias-client/src/app/layout/navigation/navigation.component.ts
+++ apps/solias-client/src/app/layout/navigation/navigation.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'solias-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
+  animations: [
+    trigger('toggle', [
+      state('true', style({ opacity: 1 })),
+      state('void', style({ opacity: 0 })),
+      transition(':enter', animate('500ms ease-in-out')),
+      transition(':leave', animate('500ms ease-in-out'))
+    ])
+  ]
 })
 export class NavigationComponent {
-  toggleShow(element: HTMLUListElement) {
-    element.classList.toggle('show')
+  isCollapsed = true;
+
+  @Input() menuItems!: any[];
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
   }
 }
