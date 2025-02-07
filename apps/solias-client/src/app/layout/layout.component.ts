cefe9a05dbd4c44c40cac4149eb11e2649a587c3
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-07 22:22:00 +0530

    Add Top Bar component

--- apps/solias-client/src/app/layout/layout.component.ts
+++ apps/solias-client/src/app/layout/layout.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from "./footer/footer.component";
 import { NAVIGATIONS_CONFIG } from '../config/navigations.config';
+import { TopBarComponent } from "./top-bar/top-bar.component";
 
 @Component({
   selector: 'solias-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, FooterComponent, TopBarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
