7bb9962d8016f55ddcb35d8a20a534b2128bb5dd
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-08 21:30:31 +0530

    Add basic top bar structure

--- apps/solias-client/src/app/layout/top-bar/top-bar.component.ts
+++ apps/solias-client/src/app/layout/top-bar/top-bar.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'solias-top-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  isLoggedIn = true;
+
+  login() {
+    console.info('Logged in');
+  }
+
+  logout() {
+    console.info('Lougout');
+  }
+}
