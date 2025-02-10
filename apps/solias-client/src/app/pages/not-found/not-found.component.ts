468860f2a5a9a79a08b1471f290ddf29df72bba2
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-11 00:05:38 +0530

    Add not found page

---
+++ apps/solias-client/src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'solias-not-found',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>not-found works!</p>`,
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {}
