2812aef2f36390bfb663401a475dfa5ddc6b75cb
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-10 22:22:00 +0530

    Add components page

---
+++ apps/solias-client/src/app/pages/components/components.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'solias-components',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './components.component.html',
+  styles: ``,
+})
+export class ComponentsComponent {}
