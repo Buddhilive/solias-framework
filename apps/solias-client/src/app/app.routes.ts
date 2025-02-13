5bc217cb299d43313c562b4dc765c7ef44d303af
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-13 07:56:06 +0530

    Remove default Nx component

--- apps/solias-client/src/app/app.routes.ts
+++ apps/solias-client/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { SoliasComponentsComponent } from '@solias-framework/solias-components';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const appRoutes: Route[] = [
     {
         path: '',
-        component: NxWelcomeComponent,
+        component: SoliasComponentsComponent,
         pathMatch: 'full'
     },
     {
