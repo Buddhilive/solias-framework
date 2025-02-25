6477728a69cab976df473bd0d021f3dcd9d97e8f
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-25 22:00:00 +0530

    Add Home component to client

--- apps/solias-client/src/app/app.routes.ts
+++ apps/solias-client/src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Route } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ComponentsComponent } from './pages/components/components.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const appRoutes: Route[] = [
     {
         path: '',
-        component: ComponentsComponent,
-        pathMatch: 'full'
-    },
-    {
-        path: 'components',
-        component: ComponentsComponent,
+        component: HomeComponent,
         pathMatch: 'full'
     },
     {
