2b303af8b00adef90c95937963aa122ba1b9d673
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-28 20:39:46 +0530

    Pages module

---
+++ apps/solias-admin/src/app/pages/page-designer/page-designer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'solias-page-designer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-designer.component.html',
+  styleUrl: './page-designer.component.scss',
+})
+export class PageDesignerComponent {}
---
+++ apps/solias-admin/src/app/pages/page-list/page-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'solias-page-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-list.component.html',
+  styleUrl: './page-list.component.scss',
+})
+export class PageListComponent {}
