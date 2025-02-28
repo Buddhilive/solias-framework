27e761fcca589d1c9511de6287f08e15df4b4d76
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-28 20:36:52 +0530

    Add post module

---
+++ apps/solias-admin/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'solias-post-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post-list.component.html',
+  styleUrl: './post-list.component.scss',
+})
+export class PostListComponent {}
