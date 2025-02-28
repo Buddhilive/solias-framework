243dc011d3debe53ea00d0d932bf614c6d1be778
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-28 18:58:10 +0530

    Add Angular SSR to Solias Client

--- apps/solias-client/src/app/app.config.ts
+++ apps/solias-client/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(),
