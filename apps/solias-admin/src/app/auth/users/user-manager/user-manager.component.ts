4bfced5129334de3e7e15388e0e820999c770626
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-02-28 20:43:19 +0530

    Add Auth module

---
+++ apps/solias-admin/src/app/auth/users/user-manager/user-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'solias-user-manager',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-manager.component.html',
+  styleUrl: './user-manager.component.scss',
+})
+export class UserManagerComponent {}
