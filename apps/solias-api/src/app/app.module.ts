5f77818408fc4d1750ad5b5ec7b388866252268a
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-03-01 19:32:57 +0530

    Use nestjs ConfigModule

--- apps/solias-api/src/app/app.module.ts
+++ apps/solias-api/src/app/app.module.ts
@@ -2,21 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: `.env.${process.env.STAGE}` }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'solias',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.STAGE ?? 'dev'}` }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     UsersModule,
   ],
