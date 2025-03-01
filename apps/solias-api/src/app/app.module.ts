ce67c13cffeb3873778e4b98d32ed57c60dcd8aa
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-03-01 19:19:39 +0530

    Setting up database connection

--- apps/solias-api/src/app/app.module.ts
+++ apps/solias-api/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.STAGE}` }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'solias',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
