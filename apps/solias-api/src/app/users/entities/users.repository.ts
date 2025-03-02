f485a2e8d2e7b4dcb2516b44a6a97b0608363c9c
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-03-02 12:48:50 +0530

    Handling missing field exception in creating user

--- apps/solias-api/src/app/users/entities/users.repository.ts
+++ apps/solias-api/src/app/users/entities/users.repository.ts
@@ -1,7 +1,12 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { ApiResponse } from '../../utils/api-response.dto';
 
@@ -11,12 +16,13 @@ export class UsersRepository extends Repository<User> {
     super(User, datasource.createEntityManager());
   }
   async createUser(user: CreateUserDto): Promise<ApiResponse<unknown>> {
-    const { email, password } = user;
+    const { username, email, password } = user;
 
     const salt = await bcrypt.genSalt(12);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = await this.create({
+      username: username,
       email: email,
       password: hashedPassword,
     });
@@ -28,8 +34,13 @@ export class UsersRepository extends Repository<User> {
         message: 'User created successfully',
       };
     } catch (error) {
+      console.log(error);
       if (error.code === '23505') {
         throw new ConflictException('Email already exist');
+      } else if (error.code === '23502') {
+        throw new BadRequestException(
+          `Database error: Required field "${error.column}" was not provided.`
+        );
       } else {
         throw new InternalServerErrorException();
       }
